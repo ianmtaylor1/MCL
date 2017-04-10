dd03d9ee1e50681a6af05a866d00ca29af1902e5
Ian Taylor <ian.m.taylor1@gmail.com>
2017-04-10 11:07:14 -0400

    Moved SqlMatrix to own repo. Added license and basic setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MCL',
+      version='1.0',
+      url='https://github.com/ianmtaylor1/MCL',
+      description='Markov Cluster Algorithm',
+      author='Ian Taylor',
+      author_email='ian.m.taylor1@gmail.com',
+      packages=['mcl'],
+      license='GNU LESSER GENERAL PUBLIC LICENSE'
+      )
